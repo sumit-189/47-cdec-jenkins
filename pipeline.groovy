6c353fb46cfbaf66912b6f5c622e202f4d0a1587
Sumit prajapati <sumitprajapati189o@gmail.com>
2026-01-26 13:15:57 +0530

    new-code added

---
+++ pipeline.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('git-pull') {
+            steps {
+                git branch: 'main', url: 'https://github.com/sumit-189/Sumit.git'
+            }
+        }
+        stage('built-stage') {
+            steps {
+               sh '''/opt/maven/bin/ mvn clean package 
+'''
+            }
+        }
+        stage('test-stage') {
+            steps {
+                echo 'code-test successfully'
+            }
+        }
+        stage('deploy-stage') {
+            steps {
+                echo 'code-deploy successfully'
+            }
+        }
+    }
+}
